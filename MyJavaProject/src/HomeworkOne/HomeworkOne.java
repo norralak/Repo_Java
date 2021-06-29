208d54e131896bd20abf527f5f03561decd5dc48
Norralak <nsukaram13@gmail.com>
2021-06-29 02:35:32 -0400

    HomeworkOne

---
+++ MyJavaProject/src/HomeworkOne/HomeworkOne.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HomeworkOne;
+
+/**
+ *
+ * @author Norralak
+ */
+public class HomeworkOne {
+    public static void main (String[] args){
+        String technoSoft = "TESTING";
+        for (int i = 0; i < technoSoft.length(); i++) {
+            System.out.println(technoSoft.charAt(i));
+        }
+    }
+}
